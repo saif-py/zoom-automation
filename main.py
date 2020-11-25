e2a879788ccd26a0542bc125120e7285ae4cd30e
saif-py <66477959+saif-py@users.noreply.github.com>
2020-11-25 13:42:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from datetime import datetime
-
+import tkinter
 import pyautogui
 
 is_class = False
@@ -16,12 +16,17 @@ while True:
         current_hrs = datetime.now().time().hour
         if int(ti[0]) == int(current_hrs):
             if int(ti[1]) == int(current_mins):
-                print(f"uh ho you've got a zoom meeting now:-- {current_hrs}:{current_mins}")
+                print(f"uh ho you've got a zoom meeting now :-- {current_hrs}:{current_mins}")
                 pyautogui.click(x=614, y=745)
                 time.sleep(1)
                 pyautogui.hotkey('windows', 'up')
                 time.sleep(1)
-                pyautogui.click(x=532, y=288)
+                root = tkinter.Tk()
+                screen_width = root.winfo_screenwidth()
+                screen_height = root.winfo_screenheight()
+                width = 0.3894 * screen_width
+                heght = 0.375 * screen_height
+                pyautogui.click(x=width, y=heght)
                 time.sleep(1)
                 pyautogui.typewrite(li)
                 pyautogui.hotkey('enter')
@@ -30,6 +35,7 @@ while True:
                 pyautogui.hotkey('enter')
                 time.sleep(1800)
             else:
+                time.sleep(58)
                 continue
         else:
             continue
